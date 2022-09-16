1cae25b820465a479110b767f35484a9794a1d37
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2022-09-16 11:12:41 -0700

    Added menu, and threading to allow for dynamic point selection

--- parking_lot_menu.py
+++ parking_lot_menu.py
@@ -11,31 +11,46 @@ class Menu():
         self.button.grid(row=1,column=0)
         self.textbox = list()
         self.size = 0
-
+        self.values = []
+        self.submitBoxes = list()
     def delete_rows(self):
-        for i in range(self.size):
+        for i in reversed(range(self.size*2)):
            self.textbox[i].grid_remove()
+        for j in reversed(range(len(self.submitBoxes))):
+            self.submitBoxes[j].grid_remove()
+
+    def submit(self):
+        print('Submitted')
+        global values
+        self.values=[x.get() for x in self.textbox]
+        values = self.values
+    def get_values(self):
+        print('Return list')
+        return self.values
 
     """   This should limit the number of boxes to self.edges """
     def press(self):
         print('Pressed')
         self.delete_rows()
         self.textbox = list()
-        size = self.edges.get()
-        self.size = int(size)
-        print(int(size))
-        for i in range(int(size)):
-            self.textbox.append(Text(self.root, height = 1, width = 57, wrap = None ))
-            self.textbox[i].grid(row=1+i+1,column=0)
-        
-        global values
-        values=[x.get(1.0, END) for x in self.textbox]
+        self.submitBoxes = list()
+        self.size = int(self.edges.get())
+        self.values=[]
+        for i in range(self.size):
+            for j in range(2):
+                self.values.append(StringVar(None))
+                col0 = Entry(self.root,textvariable=self.values[-1]) 
+                col0.insert(END, '0')
+                self.textbox.append(col0)
+                self.textbox[-1].grid(row=i+2,column=j)
+
+        self.submitBoxes.append(Button(self.root, text="Submit values", command=self.submit))
+        self.submitBoxes[-1].grid(row=(i+1*2)+3,column=0)
 
 def main():
     root = Tk()
     app = Menu(root)
     root.mainloop()
-    print(values)
 
 if __name__ == '__main__':
     main()
